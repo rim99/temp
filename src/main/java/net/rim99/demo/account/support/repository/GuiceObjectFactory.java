a8f1de04eaf73fcb5590e66f1717a07fa92a7cd5
zhangxinjason <zhangxin@outlook.com>
2018-10-21 18:27:59 +0800

    use Guice to inject private fields

---
+++ src/main/java/net/rim99/demo/account/support/repository/GuiceObjectFactory.java
@@ -0,0 +1,24 @@
+package net.rim99.demo.account.support.repository;
+
+import com.google.inject.Injector;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+
+import java.util.List;
+
+public class GuiceObjectFactory extends DefaultObjectFactory {
+
+    private Injector injector;
+
+    @Override
+    public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
+        T object = super.create(type, constructorArgTypes, constructorArgs);
+        if (injector == null) initInjector();
+        injector.injectMembers(object);
+        return object;
+    }
+
+    private synchronized void initInjector() {
+        if (injector != null) return;
+        injector = Repositories.getFactory().injector;
+    }
+}
