4681967eb6008e5fb03c642d5d0811f47a02ae2c
zhangxinjason <zhangxin@outlook.com>
2018-10-21 17:41:53 +0800

    use Log4J2 as Logger framework

--- src/test/java/net/rim99/demo/account/repository/JdbcTest.java
+++ src/test/java/net/rim99/demo/account/repository/JdbcTest.java
@@ -1,11 +1,16 @@
 package net.rim99.demo.account.repository;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JdbcTest extends RepositoryTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(JdbcTest.class);
+
     @Test
     public void test() {
-        System.out.println("Good");
+        logger.info("Good");
+        new Thread(() -> logger.info("another thread")).start();
     }
 }
